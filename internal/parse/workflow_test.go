ea0c37fce74bae7163016dd3d1ef25094bc93997
tmknom <tmk.nom@gmail.com>
2025-04-14 13:44:14 +0900

    test: add ParseAST method test in WorkflowParser

--- internal/parse/workflow_test.go
+++ internal/parse/workflow_test.go
@@ -1,6 +1,7 @@
 package parse
 
 import (
+	"github.com/google/go-cmp/cmp"
 	"strings"
 	"testing"
 
@@ -16,7 +17,6 @@ func TestWorkflowParse(t *testing.T) {
 		{"complex parameter", complexWorkflowFixture, complexWorkflowExpected},
 		{"full parameter", fullWorkflowFixture, fullWorkflowExpected},
 		{"empty parameter", emptyWorkflowFixture, emptyWorkflowExpected},
-		{"multiline parameter", multiLineWorkflowFixture, multiLineWorkflowExpected},
 		{"invalid YAML", invalidWorkflowFixture, invalidWorkflowExpected},
 	}
 
@@ -37,6 +37,93 @@ func TestWorkflowParse(t *testing.T) {
 
 }
 
+func TestWorkflowParser_ParseAST(t *testing.T) {
+	cases := []struct {
+		name     string
+		fixture  string
+		expected *WorkflowAST
+	}{
+		{
+			name:    "empty parameter",
+			fixture: emptyWorkflowFixture,
+			expected: &WorkflowAST{
+				Inputs: []*WorkflowInput{
+					{"empty", NewNullValue(), NewNullValue(), NewNullValue(), NewNullValue()},
+				},
+				Secrets:     []*WorkflowSecret{},
+				Outputs:     []*WorkflowOutput{},
+				Permissions: []*WorkflowPermission{},
+			},
+		},
+		{
+			name:    "full parameter",
+			fixture: fullWorkflowFixture,
+			expected: &WorkflowAST{
+				Inputs: []*WorkflowInput{
+					{"full-number", NewNotNullValue("5"), NewNotNullValue("The full number value."), NewNotNullValue("false"), NewNotNullValue("number")},
+				},
+				Secrets:     []*WorkflowSecret{},
+				Outputs:     []*WorkflowOutput{},
+				Permissions: []*WorkflowPermission{},
+			},
+		},
+		{
+			name:    "complex parameter",
+			fixture: complexWorkflowFixture,
+			expected: &WorkflowAST{
+				Inputs: []*WorkflowInput{
+					{"full-string", NewNotNullValue(""), NewNotNullValue("The full string value."), NewNotNullValue("true"), NewNotNullValue("string")},
+					{"full-boolean", NewNotNullValue("true"), NewNotNullValue("The full boolean value."), NewNotNullValue("false"), NewNotNullValue("boolean")},
+					{"empty", NewNullValue(), NewNullValue(), NewNullValue(), NewNullValue()},
+				},
+				Secrets:     []*WorkflowSecret{},
+				Outputs:     []*WorkflowOutput{},
+				Permissions: []*WorkflowPermission{},
+			},
+		},
+		{
+			name:    "invalid YAML",
+			fixture: invalidWorkflowFixture,
+			expected: &WorkflowAST{
+				Inputs:      []*WorkflowInput{},
+				Secrets:     []*WorkflowSecret{},
+				Outputs:     []*WorkflowOutput{},
+				Permissions: []*WorkflowPermission{},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		parser := NewWorkflowParser(conf.DefaultFormatterConfig(), conf.DefaultSortConfig())
+		got, err := parser.ParseAST(TestRawYaml(tc.fixture))
+		if err != nil {
+			t.Fatalf("%s: unexpected error: %s", tc.name, err)
+		}
+
+		if diff := cmp.Diff(got, tc.expected); diff != "" {
+			t.Errorf("%s: diff: %s", tc.name, diff)
+		}
+	}
+}
+
+const emptyWorkflowFixture = `
+on:
+  workflow_call:
+    inputs:
+      empty:
+`
+
+const fullWorkflowFixture = `
+on:
+  workflow_call:
+    inputs:
+      full-number:
+        default: 5
+        required: false
+        type: number
+        description: "The full number value."
+`
+
 const complexWorkflowFixture = `
 on:
   workflow_call:
@@ -52,40 +139,6 @@ on:
         type: boolean
         description: "The full boolean value."
       empty:
-`
-
-const fullWorkflowFixture = `
-on:
-  workflow_call:
-    inputs:
-      full-number:
-        default: 5
-        required: false
-        type: number
-        description: "The full number value."
-`
-
-const emptyWorkflowFixture = `
-on:
-  workflow_call:
-    inputs:
-      empty:
-`
-
-const multiLineWorkflowFixture = `
-on:
-  workflow_call:
-    inputs:
-      multiline-string:
-        default: |
-          {
-            "key": "value"
-          }
-        required: false
-        type: string
-        description: |
-          The multiline string.
-          Like this.
 `
 
 const invalidWorkflowFixture = `
