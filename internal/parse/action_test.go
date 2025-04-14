0a807a45a65419bf83c0403f6199cb33030e9908
tmknom <tmk.nom@gmail.com>
2025-04-14 13:31:51 +0900

    test: add ParseAST method test in ActionParser

--- internal/parse/action_test.go
+++ internal/parse/action_test.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/tmknom/actdocs/internal/conf"
 )
 
@@ -36,25 +37,94 @@ func TestActionParse(t *testing.T) {
 	}
 }
 
-const complexActionFixture = `
-name: Test Fixture
-description: This is a test Custom Action for actdocs.
+func TestActionParser_ParseAST(t *testing.T) {
+	cases := []struct {
+		name     string
+		fixture  string
+		expected *ActionAST
+	}{
+		{
+			name:    "empty parameter",
+			fixture: emptyActionFixture,
+			expected: &ActionAST{
+				Name:        NewNullValue(),
+				Description: NewNullValue(),
+				Inputs: []*ActionInput{
+					{"empty", NewNullValue(), NewNullValue(), NewNullValue()},
+				},
+				Outputs: []*ActionOutput{
+					{"only-value", NewNullValue()},
+				},
+				Runs: &ActionRuns{Using: "undefined", Steps: []*any{}},
+			},
+		},
+		{
+			name:    "full parameter",
+			fixture: fullActionFixture,
+			expected: &ActionAST{
+				Name:        NewNotNullValue("Test Fixture"),
+				Description: NewNotNullValue("This is a test Custom Action for actdocs."),
+				Inputs: []*ActionInput{
+					{"full-number", NewNotNullValue("5"), NewNotNullValue("The full number value."), NewNotNullValue("false")},
+				},
+				Outputs: []*ActionOutput{
+					{"with-description", NewNotNullValue("The Render value with description.")},
+				},
+				Runs: &ActionRuns{Using: "undefined", Steps: []*any{}},
+			},
+		},
+		{
+			name:    "complex parameter",
+			fixture: complexActionFixture,
+			expected: &ActionAST{
+				Name:        NewNotNullValue("Test Fixture"),
+				Description: NewNotNullValue("This is a test Custom Action for actdocs."),
+				Inputs: []*ActionInput{
+					{"full-string", NewNotNullValue("Default value"), NewNotNullValue("The full string value."), NewNotNullValue("true")},
+					{"full-boolean", NewNotNullValue("true"), NewNotNullValue("The full boolean value."), NewNotNullValue("false")},
+					{"empty", NewNullValue(), NewNullValue(), NewNullValue()},
+				},
+				Outputs: []*ActionOutput{
+					{"with-description", NewNotNullValue("The Render value with description.")},
+					{"only-value", NewNullValue()},
+				},
+				Runs: &ActionRuns{Using: "undefined", Steps: []*any{}},
+			},
+		},
+		{
+			name:    "invalid YAML",
+			fixture: invalidActionFixture,
+			expected: &ActionAST{
+				Name:        NewNotNullValue("Test"),
+				Description: NewNullValue(),
+				Inputs:      []*ActionInput{},
+				Outputs:     []*ActionOutput{},
+				Runs:        &ActionRuns{Using: "undefined", Steps: []*any{}},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		parser := NewActionParser(conf.DefaultFormatterConfig(), conf.DefaultSortConfig())
+		got, err := parser.ParseAST(TestRawYaml(tc.fixture))
+		if err != nil {
+			t.Fatalf("%s: unexpected error: %s", tc.name, err)
+		}
+
+		if diff := cmp.Diff(got, tc.expected); diff != "" {
+			t.Errorf("%s: diff: %s", tc.name, diff)
+		}
+	}
+}
+
+const emptyActionFixture = `
+name:
+description:
 
 inputs:
-  full-string:
-    default: "Default value"
-    required: true
-    description: "The full string value."
-  full-boolean:
-    default: true
-    required: false
-    description: "The full boolean value."
   empty:
 
 outputs:
-  with-description:
-    description: "The Render value with description."
-    value: ${{ inputs.description-only }}
   only-value:
     value: "The Render value without description."
 `
@@ -75,14 +145,25 @@ outputs:
     value: ${{ inputs.description-only }}
 `
 
-const emptyActionFixture = `
-name:
-description:
+const complexActionFixture = `
+name: Test Fixture
+description: This is a test Custom Action for actdocs.
 
 inputs:
+  full-string:
+    default: "Default value"
+    required: true
+    description: "The full string value."
+  full-boolean:
+    default: true
+    required: false
+    description: "The full boolean value."
   empty:
 
 outputs:
+  with-description:
+    description: "The Render value with description."
+    value: ${{ inputs.description-only }}
   only-value:
     value: "The Render value without description."
 `
