f0e6d1ae14357a675ff0a6d77400bb223a6b6fd2
tmknom <tmk.nom@gmail.com>
2025-04-15 20:57:44 +0900

    refactor: move conversion logic to separate _convert.go files

--- internal/format/action.go
+++ internal/format/action.go
@@ -54,31 +54,3 @@ func (f *ActionFormatter) ToMarkdown(actionSpec *ActionSpec, config *conf.Format
 	}
 	return strings.TrimSpace(sb.String())
 }
-
-func ConvertActionSpec(ast *parse.ActionAST) *ActionSpec {
-	inputs := []*ActionInputSpec{}
-	for _, inputAst := range ast.Inputs {
-		input := &ActionInputSpec{
-			Name:        inputAst.Name,
-			Default:     inputAst.Default,
-			Description: inputAst.Description,
-			Required:    inputAst.Required,
-		}
-		inputs = append(inputs, input)
-	}
-
-	outputs := []*ActionOutputSpec{}
-	for _, outputAst := range ast.Outputs {
-		output := &ActionOutputSpec{
-			Name:        outputAst.Name,
-			Description: outputAst.Description,
-		}
-		outputs = append(outputs, output)
-	}
-
-	return &ActionSpec{
-		Description: ast.Description,
-		Inputs:      inputs,
-		Outputs:     outputs,
-	}
-}
