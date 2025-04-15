80b46dd04212a4a52c2e54344a0ba431cc6779fd
tmknom <tmk.nom@gmail.com>
2025-04-16 05:41:47 +0900

    refactor: move action formatting logic to internal/action package
    - Renamed package from `format` to `action` for better separation of concerns
    - Simplified struct names by removing redundant "Action" prefix
    - Updated references in tests and orchestration logic

---
+++ internal/action/helpers_test.go
@@ -0,0 +1,11 @@
+package action
+
+import "github.com/tmknom/actdocs/internal/util"
+
+func NewNullValue() *util.NullString {
+	return util.NewNullString(nil)
+}
+
+func NewNotNullValue(value string) *util.NullString {
+	return util.NewNullString(&value)
+}
