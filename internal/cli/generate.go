e2fd5b98e32d498229d6e328e13860656167559c
tmknom <tmk.nom@gmail.com>
2025-04-15 16:16:11 +0900

    refactor: add Orchestrate method

--- internal/cli/generate.go
+++ internal/cli/generate.go
@@ -6,9 +6,6 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/tmknom/actdocs/internal/conf"
-	"github.com/tmknom/actdocs/internal/format"
-	"github.com/tmknom/actdocs/internal/parse"
-	"github.com/tmknom/actdocs/internal/read"
 )
 
 func NewGenerateCommand(formatterConfig *conf.FormatterConfig, sortConfig *conf.SortConfig, io *IO) *cobra.Command {
@@ -48,35 +45,9 @@ type GenerateOption struct {
 }
 
 func (r *GenerateRunner) Run() error {
-	reader := &read.SourceReader{}
-	yaml, err := reader.Read(r.source)
+	formatted, err := Orchestrate(r.source, r.FormatterConfig, r.SortConfig)
 	if err != nil {
 		return err
-	}
-	log.Printf("read: %s", r.source)
-
-	factory := &parse.ParserFactory{Raw: yaml}
-	parser, err := factory.Factory(r.FormatterConfig, r.SortConfig)
-	if err != nil {
-		return err
-	}
-	log.Printf("selected parser: %T", parser)
-
-	content, err := parser.ParseAST(yaml)
-	if err != nil {
-		return err
-	}
-
-	formatted := ""
-	switch content.(type) {
-	case *parse.ActionAST:
-		formatter := format.NewActionFormatter(r.FormatterConfig)
-		formatted = formatter.Format(content.(*parse.ActionAST))
-	case *parse.WorkflowAST:
-		formatter := format.NewWorkflowFormatter(r.FormatterConfig)
-		formatted = formatter.Format(content.(*parse.WorkflowAST))
-	default:
-		return fmt.Errorf("unsupported AST type: %T", content)
 	}
 
 	_, err = fmt.Fprintln(r.OutWriter, formatted)
--- internal/cli/inject.go
+++ internal/cli/inject.go
@@ -11,9 +11,6 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/tmknom/actdocs/internal/conf"
-	"github.com/tmknom/actdocs/internal/format"
-	"github.com/tmknom/actdocs/internal/parse"
-	"github.com/tmknom/actdocs/internal/read"
 )
 
 func NewInjectCommand(formatter *conf.FormatterConfig, sort *conf.SortConfig, io *IO) *cobra.Command {
@@ -60,35 +57,9 @@ type InjectOption struct {
 }
 
 func (r *InjectRunner) Run() error {
-	reader := &read.SourceReader{}
-	yaml, err := reader.Read(r.source)
+	formatted, err := Orchestrate(r.source, r.FormatterConfig, r.SortConfig)
 	if err != nil {
 		return err
-	}
-	log.Printf("read: %s", r.source)
-
-	factory := &parse.ParserFactory{Raw: yaml}
-	parser, err := factory.Factory(r.FormatterConfig, r.SortConfig)
-	if err != nil {
-		return err
-	}
-	log.Printf("selected parser: %T", parser)
-
-	content, err := parser.ParseAST(yaml)
-	if err != nil {
-		return err
-	}
-
-	formatted := ""
-	switch content.(type) {
-	case *parse.ActionAST:
-		formatter := format.NewActionFormatter(r.FormatterConfig)
-		formatted = formatter.Format(content.(*parse.ActionAST))
-	case *parse.WorkflowAST:
-		formatter := format.NewWorkflowFormatter(r.FormatterConfig)
-		formatted = formatter.Format(content.(*parse.WorkflowAST))
-	default:
-		return fmt.Errorf("unsupported AST type: %T", content)
 	}
 
 	file, err := os.Open(r.OutputFile)
